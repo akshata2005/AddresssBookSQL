2a5da88f07da352a8b9b828839396f1d8371f7ee
akshata2005 <akshatasawant1805@gmail.com>
2022-02-17 23:07:59 +0530

    [Akshata] Add: retrive data from the table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -28,3 +28,20 @@ WHERE first_name = 'Pranoti' AND last_name= 'Sawant';
 
 DELETE FROM address_Book
 WHERE first_name = 'Rutuja' AND last_name='Pati';
+
+INSERT INTO address_Book(first_name,last_name,address,city,state,zip,phone,email)
+VALUES ('Ashish','Sawant','Koregaon Park','Pune','Maharashtra',410038,7865456787,'Ashishsawant23@gmail.com'),
+('Ayush','Rajput','Kashima Nagar','Aurangabad','Maharashtra',431001,7658768987,'rajputAuysh12@gmail.com'),
+('Akash','Girase','Gulmohar Nagar','Pansemal','U.P',4551001,9921366811,'Akash21@gmail.com'),
+('Shivani','Joshi','krishna Colony','Ahemdabad','Gujraat',320008,8553109765,'shivanijoshi2604@gmail.com');
+
+SELECT * FROM address_Book
+WHERE city = 'Pune'
+
+SELECT * FROM address_Book
+WHERE state = 'Gujraat'
+
+SELECT COUNT(*) FROM address_Book
+WHERE city = 'Pune'
+
+SELECT COUNT(*) FROM address_Book
